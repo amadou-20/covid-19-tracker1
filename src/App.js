02e486dc3938ce70414ddc5be8c6b35e27f085f5
amadou-20 <ahmo300@outlook.com>
2021-01-02 12:36:20 +0100

    Now we have the select items here

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
 import React from "react";
 import './App.css';
+import { MenuItem,FormControl,Select } from "@material-ui/core";
+
 
 function App() {
+
+  
   return (
     <div className="app">
-      <h1>Hello world !!!</h1>
+      <div className="app_header">
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl className="app_dropdown">
+        <Select variant="outlined" value="abc">
+          <MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Worldwide 2</MenuItem>
+          <MenuItem value="worldwide">Worldwide3</MenuItem>
+          <MenuItem value="worldwide">Worldwide 4</MenuItem>
+        </Select>
+      </FormControl>
+      </div>     
     </div>
   );
 }
