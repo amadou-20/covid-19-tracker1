056e12a3af5037bea596e802770dab941055aa4a
amadou-20 <ahmo300@outlook.com>
2021-01-02 18:15:48 +0100

    I put a style to the table component

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
   const [countries, setCountries] = useState([]);
   const [country, setCountry] = useState("worldwide");
   const [countryInfo, setCountryInfo] = useState({});
+  const [tableData, setTableData] = useState([]);
 
   useEffect(() => {
     fetch("https://disease.sh/v3/covid-19/all")
@@ -30,6 +31,7 @@ function App() {
            value: country.countryInfo.iso2 // USA, UK, FR
            }));
   
+        setTableData(data);   
         setCountries(countries);
          });
       }
@@ -85,7 +87,7 @@ function App() {
         <Card className="app_right">
            <CardContent>
              <h3>Live Cases by Country</h3>
-              <Table />
+              <Table countries={tableData} />
              <h3>worldwide new cases</h3>
               {/* Graph */}
            </CardContent>
