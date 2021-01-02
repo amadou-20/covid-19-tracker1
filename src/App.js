2303eccad5c996899e3ab35ec8985bed7b671ebb
amadou-20 <ahmo300@outlook.com>
2021-01-02 13:32:43 +0100

    I get back all the countries now

--- src/App.js
+++ src/App.js
@@ -1,13 +1,28 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import './App.css';
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 
 function App() {
-
   const [countries, setCountries] = useState([
-    "USA", "UK", "JAPON", "MALI",
-  ]);
+    "USA", "UK", "JAPON", "MALI",]);
+
+    useEffect(() => {
+      const getCountriesData = async() => {
+      await  fetch("https://disease.sh/v3/covid-19/countries")
+         .then((reponse) => reponse.json())
+         .then((data) => {
+           const countries = data.map((country) => ({
+           name: country.country, // united states, united kingdom
+           value: country.countryInfo.iso2 // USA, UK, FR
+           }));
+  
+        setCountries(countries);
+         });
+      }
+      getCountriesData();
+    }, []);
+
 
   return (
     <div className="app">
@@ -18,7 +33,7 @@ function App() {
           <MenuItem value="worldwide">Worldwide</MenuItem>
           {
             countries.map((country) => (
-              <MenuItem value={country} >{country}</MenuItem>
+              <MenuItem value={country.value} >{country.name}</MenuItem>
             ))}
         </Select>
       </FormControl>
